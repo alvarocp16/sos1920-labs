1f176db27d4849067bc85296c3a72689b479f780
alvarocp16 <alvarocp16@gmail.com>
2020-03-10 15:52:17 +0000

    l05 finish

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require("express");
 
 var app = express();
 
+app.use("/",express.static("./public"));
+
 app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
-app.listen(80);
+app.listen(80, () => {
+	console.log("Server ready");
+});
 
-console.log("server ready");
+console.log("Stating server...");
