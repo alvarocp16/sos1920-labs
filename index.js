958e8d6f809aef1cea7dcb1774150a9be7ff6f67
alvarocp16 <alvarocp16@gmail.com>
2020-03-10 11:14:21 +0000

    final version l04

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const cool = require("cool-ascii-faces");
 
-console.log(cool());
+const express = require("express");
+
+var app = express();
+
+app.get("/cool",(request,response) => {
+	response.send("<html>"+cool()+"</html>");
+});
+
+app.listen(80);
+
+console.log("server ready");
